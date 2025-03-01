96656c171d5e30c491f6f86a5ad36ac80afb1ab2
ErDanieh <danienclase5@gmail.com>
2025-03-01 16:41:25 +0100

    Cambios en el modelo de preguntas

--- questionservice/question-model.js
+++ questionservice/question-model.js
@@ -1,75 +1,19 @@
 const mongoose = require('mongoose');
 
-//preguntas
-const preguntaSchema = new mongoose.Schema({
-    textoPregunta_es: {
-        type: String,
-        required: true
-    },
-    respuestaCorrecta_es: {
-    type: String,
-        required: true
-    },
-    textoPregunta_en: {
+
+const monumentSchema = new mongoose.Schema({
+  monumento: {
       type: String,
       required: true
-    },
-    respuestaCorrecta_en: {
-    type: String,
-        required: true
-    },
-    categoria: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Categoria'
-    }
-    });
-
-const Pregunta = mongoose.model('Pregunta', preguntaSchema);
-
-//categoria
-const categoriaSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true
-  }
-});
-
-const Categoria = mongoose.model('Categoria', categoriaSchema);
-
-//Respuesta
-const respuestaSchema = new mongoose.Schema({
-  textoRespuesta_es: {
-    type: String,
-    required: true
   },
-  textoRespuesta_en: {
-    type: String,
-    required: true
-  },
-  tipos: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Tipos'
-  }]
-});
-const Respuesta = mongoose.model('Respuesta', respuestaSchema);
-
-//Tipos
-const tiposSchema = new mongoose.Schema({
-  idPreguntas: [{
-    type: String,
-    required: true
-  }],
-  nombreTipo: {
-    type: String,
-    required: true
+  pais: {
+      type: String,
+      required: false
   }
-});
+}, {timestamps: {}});
 
-// Definir el modelo de respuesta
-const Tipos = mongoose.model('Tipos', tiposSchema);
+const Monumento = mongoose.model('Monument', monumentSchema);
+
 module.exports = {
-  Pregunta,
-  Categoria,
-  Respuesta,
-  Tipos
+  Monumento
 };
