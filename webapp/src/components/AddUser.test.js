72917a11c80cdc9947caf5c83edd9db3669229e4
Adrián Martís Blanco <36863094+adriMB200@users.noreply.github.com>
2025-03-06 23:16:12 +0100

    Añadidos los test de addUser que comprueban el funcionamiento de las validaciones en username y password

--- webapp/src/components/AddUser.test.js
+++ webapp/src/components/AddUser.test.js
@@ -56,4 +56,149 @@ describe('AddUser component', () => {
       expect(screen.getByText(/Error: Internal Server Error/i)).toBeInTheDocument();
     });
   });
+
+
+  //Test de mensajes de error 
+
+  it('should show error for empty username', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: '' } });
+    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/Both username and password are required/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should show error for empty password', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: 'testPassword' } });
+    fireEvent.change(passwordInput, { target: { value: '' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/Both username and password are required/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should show error for less lenght username', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: 'as' } });
+    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/The username length cannot be less than 3 characters/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should show error for more lenght username', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: 'asasdasfsadfsdafgsagasdgasdgasgsagsagsagdsagds' } });
+    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/The username cannot have more than 20 characters/i)).toBeInTheDocument();
+    });
+  });
+
+
+  
+  it('should show error for less lenght password', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: 'as2312' } });
+    fireEvent.change(passwordInput, { target: { value: 'te' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/The password length cannot be less than 4 characters/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should show error for more lenght password', async () => {
+    render(<AddUser />);
+
+    const usernameInput = screen.getByLabelText(/Username/i);
+    const passwordInput = screen.getByLabelText(/Password/i);
+    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+
+    // Mock the axios.post request to simulate a successful response
+    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+
+    // Simulate user input
+    fireEvent.change(usernameInput, { target: { value: 'asasdas' } });
+    fireEvent.change(passwordInput, { target: { value: 'asasdasfsadfsdafgsagasdgasdgasgsagsagsagdsagds' } });
+
+    // Trigger the add user button click
+    fireEvent.click(addUserButton);
+
+    // Wait for the Snackbar to be open
+    await waitFor(() => {
+      expect(screen.getByText(/The password cannot have more than 20 characters/i)).toBeInTheDocument();
+    });
+  });
+
+
 });
