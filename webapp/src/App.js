8e75403e991b9b1871d4c16437fa62ec0fb3bcbd
ErDanieh <danienclase5@gmail.com>
2025-03-06 16:43:53 +0100

    Esbozo de prueba de wikidata en Frontend

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import AddUser from './components/AddUser';
-import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { Typewriter } from "react-simple-typewriter";
 
-function App() {
+import AddUser from './components/AddUser';
+import Login from './components/Login';
+
+
+function Home() {
   const [showLogin, setShowLogin] = useState(true);
 
   const handleToggleView = () => {
@@ -17,7 +22,7 @@ function App() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-        Welcome to the 2025 edition of the Software Architecture course
+        Wikidata test page for Wichat6b
       </Typography>
       {showLogin ? <Login /> : <AddUser />}
       <Typography component="div" align="center" sx={{ marginTop: 2 }}>
@@ -32,6 +37,42 @@ function App() {
         )}
       </Typography>
     </Container>
+  );
+}
+
+function App() {
+
+  
+  const [showLogin, setShowLogin] = useState(true);
+
+  const handleToggleView = () => {
+    setShowLogin(!showLogin);
+  };
+
+  return (
+    <Router>
+      <>
+        <Container component="header" maxWidth={false} 
+          sx={{
+            backgroundColor: "#FFFDD0",
+            margin: 0,
+            height: "5vh",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            paddingBottom: 10,
+          }}
+        >
+          <Typography component="h3" variant="h4" align="center" sx={{ marginTop: 2 }}>
+            <Typewriter words={["Wikidata trial page for wichat6b tests"]} cursor cursorStyle="|" typeSpeed={50} />
+          </Typography>
+        </Container>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
