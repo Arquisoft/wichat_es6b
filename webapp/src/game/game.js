202ad2c70e322a8ac1294773751c3b70b9d4ea76
ErDanieh <danienclase5@gmail.com>
2025-03-27 20:43:52 +0100

    Problema de preguntas no correspondientes a tematica arreglado

--- webapp/src/game/game.js
+++ webapp/src/game/game.js
@@ -7,25 +7,30 @@ class Game {
   }
 
   async fetchQuestions() {
+    console.log("Fetching questions..."); // <-- Agrega esto
     this.questions = []; // Reset antes de hacer nuevas llamadas
-    const urls = [
-      "http://localhost:8010/generateQuestion?language=es&thematic=Geografia",
-      "http://localhost:8010/generateQuestion?language=es&thematic=Geografia",
-      "http://localhost:8010/generateQuestion?language=es&thematic=Geografia",
-      "http://localhost:8010/generateQuestion?language=es&thematic=Geografia"
-    ];
+    let tipoPreguntas=["Geografia","Cultura","Personajes"]
+    const urls = Array.from({ length: 4 }, () => {
+      const tipoAleatorio = tipoPreguntas[Math.floor(Math.random() * tipoPreguntas.length)];
+      return `http://localhost:8010/generateQuestion?language=es&thematic=${tipoAleatorio}`;
+    });
 
     try {
-      const responses = await Promise.all(urls.map(url => axios.get(url)));
+      for (let [index, url] of urls.entries()) {
+        const response = await axios.get(url);
 
-      this.questions = responses.map((response, index) => ({
-        id: `q${index + 1}`,
-        pregunta: response.data.responseQuestion,
-        opciones: response.data.responseOptions,
-        respuesta_correcta: response.data.responseOptions.indexOf(response.data.responseCorrectOption),
-        imagen: response.data.responseImage, // Agregamos la imagen
-      }));
+    
+        this.questions.push({
+          id: `q${index + 1}`,
+          pregunta: response.data.responseQuestion,
+          opciones: response.data.responseOptions,
+          respuesta_correcta: response.data.responseOptions.indexOf(response.data.responseCorrectOption),
+          imagen: response.data.responseImage, // Agregamos la imagen
+        });
+        console.log("Pregunta "+index+" realizada");
+      }
 
+      console.log(this.questions)
       return this.questions;
     } catch (error) {
       console.error("Error fetching questions:", error);
