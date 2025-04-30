2b30bd2ed0c73d2404babbfe1d671ed8d117e583
Adrián Martís Blanco <36863094+adriMB200@users.noreply.github.com>
2025-04-30 17:24:16 +0200

    Eliminacion de codigo duplicado

--- webapp/src/components/AddUser.test.js
+++ webapp/src/components/AddUser.test.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import AddUser from './AddUser';
 import { MemoryRouter } from 'react-router-dom';
-import { SessionContext } from '../context/SessionContext'; // Import the SessionContext
+import { SessionContext } from '../context/SessionContext';
 
 const mockAxios = new MockAdapter(axios);
 
@@ -13,73 +13,55 @@ describe('AddUser component', () => {
     mockAxios.reset();
   });
 
-  it('should add user successfully', async () => {
-    const createSession = jest.fn();
-    render(
-      <SessionContext.Provider value={{ createSession }}>
+  const renderComponent = (sessionValue) => {
+    return render(
+      <SessionContext.Provider value={sessionValue}>
         <MemoryRouter>
           <AddUser />
         </MemoryRouter>
       </SessionContext.Provider>
     );
+  };
+
+  it('should add user successfully', async () => {
+    const createSession = jest.fn();
+    renderComponent({ createSession });
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const passwordInput = screen.getByLabelText(/Password/i);
     const addUserButton = screen.getByRole('button', { name: /Register/i });
 
-    // Mock the axios.post request to simulate a successful response
     mockAxios.onPost('http://localhost:8000/adduser').reply(200);
 
-    // Simulate user input
     fireEvent.change(usernameInput, { target: { value: 'testUser' } });
     fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-
-    // Trigger the add user button click
     fireEvent.click(addUserButton);
 
-    // Wait for the Snackbar to be open
     await waitFor(() => {
       expect(screen.getByText(/User added successfully/i)).toBeInTheDocument();
     });
   });
 
   it('should handle error when adding user', async () => {
-    render(
-      <SessionContext.Provider value={{}}>
-        <MemoryRouter>
-          <AddUser />
-        </MemoryRouter>
-      </SessionContext.Provider>
-    );
+    renderComponent({});
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const passwordInput = screen.getByLabelText(/Password/i);
     const addUserButton = screen.getByRole('button', { name: /Register/i });
 
-    // Mock the axios.post request to simulate an error response
     mockAxios.onPost('http://localhost:8000/adduser').reply(500, { error: 'Internal Server Error' });
 
-    // Simulate user input
     fireEvent.change(usernameInput, { target: { value: 'testUser' } });
     fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-
-    // Trigger the add user button click
     fireEvent.click(addUserButton);
 
-    // Wait for the error Snackbar to be open
     await waitFor(() => {
       expect(screen.getByText(/internal server error/i)).toBeInTheDocument();
     });
   });
 
   it('should not call the API if username is empty', async () => {
-    render(
-      <SessionContext.Provider value={{}}>
-        <MemoryRouter>
-          <AddUser />
-        </MemoryRouter>
-      </SessionContext.Provider>
-    );
+    renderComponent({});
 
     const passwordInput = screen.getByLabelText(/Password/i);
     const addUserButton = screen.getByRole('button', { name: /Register/i });
@@ -87,23 +69,15 @@ describe('AddUser component', () => {
     fireEvent.change(passwordInput, { target: { value: 'validPassword' } });
     fireEvent.click(addUserButton);
 
-    // Wait for the error Snackbar to be open
     await waitFor(() => {
       expect(screen.getByText(/Both username and password are required/i)).toBeInTheDocument();
     });
 
-    // Ensure the API call was not made
     expect(mockAxios.history.post.length).toBe(0);
   });
 
   it('should not call the API if password is empty', async () => {
-    render(
-      <SessionContext.Provider value={{}}>
-        <MemoryRouter>
-          <AddUser />
-        </MemoryRouter>
-      </SessionContext.Provider>
-    );
+    renderComponent({});
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const addUserButton = screen.getByRole('button', { name: /Register/i });
@@ -111,40 +85,29 @@ describe('AddUser component', () => {
     fireEvent.change(usernameInput, { target: { value: 'validUser' } });
     fireEvent.click(addUserButton);
 
-    // Wait for the error Snackbar to be open
     await waitFor(() => {
       expect(screen.getByText(/Both username and password are required/i)).toBeInTheDocument();
     });
 
-    // Ensure the API call was not made
     expect(mockAxios.history.post.length).toBe(0);
   });
 
   it('should clear the validation error message after valid input', async () => {
-    render(
-      <SessionContext.Provider value={{}}>
-        <MemoryRouter>
-          <AddUser />
-        </MemoryRouter>
-      </SessionContext.Provider>
-    );
+    renderComponent({});
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const passwordInput = screen.getByLabelText(/Password/i);
     const addUserButton = screen.getByRole('button', { name: /Register/i });
 
-    // Trigger a validation error
     fireEvent.click(addUserButton);
     await waitFor(() => {
       expect(screen.getByText(/Both username and password are required/i)).toBeInTheDocument();
     });
 
-    // Now provide valid input
     fireEvent.change(usernameInput, { target: { value: 'validUser' } });
     fireEvent.change(passwordInput, { target: { value: 'validPassword' } });
     fireEvent.click(addUserButton);
 
-    // Wait for a short time to see if the error message disappears (adjust timeout if needed)
     await waitFor(() => {
       expect(screen.queryByText(/Both username and password are required/i)).not.toBeInTheDocument();
     }, { timeout: 100 });
