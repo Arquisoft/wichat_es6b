9227a8e559f92edab4e4c320e5a0d52879a3b68a
david <davidrch03@gmail.com>
2025-05-01 20:24:36 +0200

    Se añade mas pruebas

--- webapp/e2e/steps/login-form.steps.js
+++ webapp/e2e/steps/login-form.steps.js
@@ -44,33 +44,6 @@ defineFeature(feature, test => {
     });
 
     when('I fill the data in the form and press login', async () => {
-      // Click on Signup tab
-      await page.waitForFunction(xpath => {
-        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        return element !== null;
-      }, {}, '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div/button[2]');
-      
-      await page.evaluate(xpath => {
-          const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-          if (element) element.click();
-      }, '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div/button[2]');
-
-      // Fill registration form
-      await page.type('input[id="usernameAWA"]', username);
-      await page.type('input[id="passwordAWA"]', password);
-      await page.type('input[id="passwordConfirmAWA"]', password);
-      await page.click('button[id="botonAWA"]');
-
-      // Click on Signup tab
-      await page.waitForFunction(xpath => {
-        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        return element !== null;
-    }, {}, '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div/button[1]');
-    
-    await page.evaluate(xpath => {
-        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (element) element.click();
-    }, '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div/button[1]');
 
       await expect(page).toFill('input[id="usernameLoginw"]', username);
       await expect(page).toFill('input[id="passwordLoginw"]', password);
