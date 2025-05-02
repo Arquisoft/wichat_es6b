9d375fdcdb8425402a89cdfc7e61c563e9d25e97
Sero73 <seroplaye@gmail.com>
2025-05-02 18:26:50 +0200

    masTests

--- webapp/src/game/gameManager.test.js
+++ webapp/src/game/gameManager.test.js
@@ -21,41 +21,6 @@ describe('Game class', () => {
     expect(game.score).toBe(0);
   });
 
-//  it('should fetch questions successfully', async () => {
-//   const mockQuestions = [
-//     {
-//       id: 'q1',
-//       pregunta: '¿Cuál es la capital de España?',
-//       opciones: ['Madrid', 'Barcelona', 'Sevilla', 'Valencia'],
-//       respuesta_correcta: 0,
-//       imagen: 'imagen1.jpg',
-//       tipo: 'Geografia', // Nota: El tipo aquí es Geografia
-//     }
-//   ];
-
-//   // Mockea la respuesta de axios.get
-//   axios.get.mockResolvedValueOnce({ data: mockQuestions });
-
-//   const mockCallback = jest.fn();
-
-//   // Llama a la función que quieres probar
-//   // Asumiendo que fetchQuestions no necesita argumentos específicos para la temática en este test
-//   await game.fetchQuestions(mockCallback);
-
-//   // --- Corrección aquí ---
-//   // Verifica que axios.get fue llamado con la URL y el objeto de parámetros correctos
-//   expect(axios.get).toHaveBeenCalledWith(
-//     `${apiEndpoint}/generateQuestions`, // Primer argumento: la URL base
-//     { // Segundo argumento: el objeto de configuración
-//       params: { // Dentro de la configuración, el objeto params
-//         language: "es",
-//         thematic: "Geografia",
-//       },
-//     signal: expect.any(AbortSignal), // Verifica que se pase una señal de AbortController
-//     }
-//   );
-// });
-
   it('should handle error when fetching questions', async () => {
     axios.get.mockRejectedValueOnce(new Error('Network Error')); 
     const mockCallback = jest.fn();
@@ -125,17 +90,4 @@ describe('Game class', () => {
     expect(game.checkAnswer(10, 0)).toBe(false);
     expect(game.score).toBe(0);
   });
-
-//   it('should handle multiple categories correctly', async () => {
-//     const game = new Game(['Futbolistas', 'Pintores', 'Cantantes']);
-//     axios.get.mockResolvedValueOnce({ data: [] });
-    
-//     await game.fetchQuestions(() => {});
-    
-//     expect(axios.get).toHaveBeenCalledWith(
-//   "http://localhost:8000/generateQuestions",
-//   { params: { language: "es", thematic: "Futbolistas" } }
-//     );
-//   });
-
 });
